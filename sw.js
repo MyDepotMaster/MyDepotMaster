7290ac4623d26ac42ac99d67c8b86e39c80c41f0
MyDepotMaster <chongos909@gmail.com>
2026-06-11 12:02:25 +0200

    Add files via upload
    Update

--- sw.js
+++ sw.js
@@ -84,7 +84,7 @@
 
 // ────────────────────────────────────────────────────────────────────────────
 
-const CACHE     = 'mdm-v21';   // ← bump this whenever you deploy a new version
+const CACHE     = 'mdm-v22';   // ← bump this whenever you deploy a new version
 const SHELL     = './';
 const FONTS_CSS = 'https://fonts.googleapis.com/css2?family=DM+Sans:ital,opsz,wght@0,9..40,300;0,9..40,400;0,9..40,500;0,9..40,600;0,9..40,700;1,9..40,400&family=DM+Mono:wght@400;500&family=Syne:wght@600;700;800&display=swap';
 
